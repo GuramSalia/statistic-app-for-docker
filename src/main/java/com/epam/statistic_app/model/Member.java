140beecf78da0af01e100b6843567119eed48c63
Guram Salia <guramsalia@gmail.com>
2024-07-26 23:47:37 +0400

    modify Member class to comment out mongo db related code

--- src/main/java/com/epam/statistic_app/model/Member.java
+++ src/main/java/com/epam/statistic_app/model/Member.java
@@ -1,8 +1,8 @@
 package com.epam.statistic_app.model;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -10,14 +10,14 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "members")
+//@Document(collection = "members")
 @Getter
 @Setter
 public class Member implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @Id
+//    @Id
     private String id;
     private String username;
 
